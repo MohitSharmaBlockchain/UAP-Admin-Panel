09d360675db009349610f2e6a3658b6d6ee2e6f9
Karan-Dhingra <Dhingrakaran764@gmail.com>
2022-02-01 23:12:31 +0530

    format csv

--- src/DownloadAllCSV.jsx
+++ src/DownloadAllCSV.jsx
@@ -17,10 +17,8 @@ const exportToCsv = (e, data__from__web) => {
     e.preventDefault()
 
     // Headers for each column
-    let USER__ID
     let head = ''
     let arr = []
-    // data__from__web.forEach((element) => {
     for (const key in data__from__web[0]) {
         if (
             key !== 'password' &&
@@ -34,66 +32,42 @@ const exportToCsv = (e, data__from__web) => {
             head += key.toUpperCase() + ','
             arr.push(key)
         }
-        // if (key === '_id') {
-        //     USER__ID = element[key]
-        //     console.log('Element ID: ' + element[key])
-        // }
     }
-    // })
-    // console.log(head)
-    // console.log(arr)
     let headers = [head]
 
-    // Convert users data to a csv
-    // let i = 1
     let usersCsv = data__from__web.reduce((acc, user) => {
         let row = ''
-        for (let key in user) {
-            for (let i in arr)
+        for (let i in arr) {
+            let search = false
+            for (let key in user) {
                 if (key === arr[i]) {
-                    row += user[key] + ','
-                    // console.log(`USER: ${i}:`, user)
-                    // const { id, name, surname, age } = user
-                    // acc.push([id, name, surname, age].join(','))
-                    // console.log(`USER: ${i}:`, acc)
-                    // i++
-                    console.log('CHECK')
+                    search = true
                     break
                 }
-            // console.log(row)
+            }
+            if (search) {
+                let text = user[arr[i]]
+                let word__data = ''
+                if (typeof text === 'string') {
+                    for (let i = 0; i < text.length; i++) {
+                        if (text[i] === ',' || text[i] === '\n') continue
+                        word__data += text[i]
+                    }
+                    row += word__data + ','
+                } else {
+                    row += text + ','
+                }
+
+                // console.log(word__data)
+            } else {
+                // console.log(user['_id'])
+                row += ','
+            }
         }
         acc.push(row)
         return acc
     }, [])
 
-    console.log(usersCsv)
-
-    // let usersCsv = []
-
-    // let body = ''
-    // data__from__web.forEach((element) => {
-    //     if (
-    //         element[0] !== 'password' &&
-    //         element[0] !== 'backgroundUrl' &&
-    //         element[0] !== '__v' &&
-    //         element[0] !== 'tags' &&
-    //         element[0] !== 'jsonHash' &&
-    //         element[0] !== 'cloudinaryUrl'
-    //     ) {
-    //         body +=
-    //             (element[1] === true
-    //                 ? 'YES'
-    //                 : element[1] === false
-    //                 ? 'NO'
-    //                 : element[1] === ''
-    //                 ? '--'
-    //                 : element[1]) + ','
-    //     }
-    // })
-
-    // usersCsv.push(body)
-
-    // console.log(usersCsv)
     downloadFile({
         data: [...headers, ...usersCsv].join('\n'),
         fileName: `data.csv`,
